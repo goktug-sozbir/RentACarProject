5fa9800d6fdfb4579fc78207d69281d35e74e844
Hilmi Göktuğ Sözbir <goktug.szbr@hotmail.com>
2021-10-03 17:22:47 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,49 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.Concrete.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public void Add(Car car)
+        {
+            if (car.CarName.Length>=2 && car.DailyPrice>0)
+            {
+                _carDal.Add(car);
+                Console.WriteLine(car + " eklendi.");
+            }
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+        public List<CarDetailDto> GetCarDetails()
+        {
+            return _carDal.GetCarDetails();
+        }
+        // CarName = c.CarName, BrandName=b.BrandName, ColorName=color.ColorName, DailyPrice=c.DailyPrice
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(p => p.BrandId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(p => p.ColorId == id);
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
+
+namespace ConsoleUI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            foreach (var cars in carManager.GetCarDetails())
+            {
+                Console.WriteLine(cars.CarName + " " + cars.BrandName + " " + cars.ColorName + " " + cars.DailyPrice);
+            }
+        }
+    }
+}
